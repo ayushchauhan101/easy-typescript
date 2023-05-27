a72e30adb175a11af4687d55d748c5ca6b01d6bb
ayush <ayush.chauhan0008@gmail.com>
2023-05-28 01:11:21 +0530

    adding a calcalutor

--- index.ts
+++ index.ts
@@ -1,5 +1,32 @@
-const multiplier = (a, b, printText) => {
-	console.log(printText, a * b)
+// Newly created type i.e 'Operation' will only accept these following strings
+type Operation = "multiply" | "add" | "divide"
+
+type ResultType = string | number
+
+// using switch case
+const calculator = (a: number, b: number, op: Operation): number => {
+	switch (op) {
+		case "multiply":
+			return a * b
+		case "add":
+			return a + b
+		case "divide":
+			if (b === 0) {
+				throw new Error("cannot divide by 0")
+			}
+		default:
+			throw new Error("Not add, divide or multiply operation")
+	}
 }
 
-multiplier(2,4,'Multiply 2 and 4, and the result is:')
+console.log(calculator(1, 2, "add"))
+
+// using if else
+// const calculator = (a: number, b: number, op: Operation): ResultType => {
+// 	if (op === "multiply") return a * b
+// 	else if (op === "add") return a + b
+// 	else if (op === "divide") {
+// 		if (b === 0) return "cannot divide by 0"
+// 		return a / b
+// 	}
+// }
