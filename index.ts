902fd6c8c906bf42bd61d1df05d6cb9201a28b5c
ayush <ayush.chauhan0008@gmail.com>
2023-05-30 14:05:52 +0530

    more examples.

--- index.ts
+++ index.ts
@@ -1,4 +1,23 @@
-const greet : string = 'hello'
+console.log("this is a typescript program");
 
-console.log('this is a typescript program')
-console.log(`${greet} world!`)
+const greet: string = "hello";
+console.log(`${greet} world!`);
+
+// create a function
+function addOne(age: number): number {
+	return age + 1;
+}
+
+console.log(addOne(32));
+
+// variable with multiple types; myName can store two datatypes
+let myName: string | number;
+myName = "Boston Red Sox";
+// myName = 45;
+console.log(myName, "is a", typeof myName);
+
+// creating Object literal types
+type Answer = { a: number; b: string };
+const val1: Answer = { a: 5, b: "hello" };
+
+console.log(val1);
