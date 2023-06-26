2fe6ae5c16fc07ccb2ecbb230c5527877a284460
ayush <ayush.chauhan0008@gmail.com>
2023-06-26 12:24:32 +0530

    intersection types

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # easy-typescript
 
-### Part5 - Type Aliases & Union types
+### Part5 - Type Aliases, Union and Intersection type
 
 **Type alias:**
 
@@ -25,14 +25,24 @@ let user1: CustomType = {
 
 **Union type:**
 
-- To assign a type with combination of multiple types
+- To assign a type with combination of multiple types using `|` between types
 
 ```ts
-let password: number | string = "123sdhkj"
+let password: number | string = "ballerophone"
 OR
 let password: number | string = 123455
 ```
 
 - here, the variable password can take either number or string type
+
+**Type intesection**
+
+- Variable having combination of multiple types using `&` between types
+
+```ts
+type EmployeeNumber = Employee && number
+```
+
+- here, Employee is an interface
 
 _This is an ongoing project and the maintainer appreciate any and all input, issues, discussions as well as PR(Pull Request)_
--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
-// // Type alias
-// type Employee = {
-// 	readonly id: number
-// 	name: string
-// 	age?: number
-// }
+// Type alias
+type Employee = {
+	readonly id: number
+	name: string
+	age?: number
+}
 
-// let employee1: Employee = {
-// 	id: 1234,
-// 	name: "sarah",
-// }
+let employee1: Employee = {
+	id: 1234,
+	name: "sarah",
+}
 
-// let employee2: Employee = {
-// 	id: 3456,
-// 	name: "tommy",
-// 	age: 38,
-// }
+let employee2: Employee = {
+	id: 3456,
+	name: "tommy",
+	age: 38,
+}
 
-// console.log(employee1)
-// console.log(employee2)
+console.log(employee1)
+console.log(employee2)
 
 // union type
 function mTokm(length: number | string): number {
@@ -32,3 +32,26 @@ function mTokm(length: number | string): number {
 console.log(mTokm(1000), "kilometer")
 console.log(mTokm("8000"), "kilometer")
 console.log(mTokm("50"), "kilometer")
+
+// intersection type
+type Start = {
+	engineOn: boolean
+	driverName: string
+}
+
+type Move = {
+	startPoint: number
+	endPoint: number
+}
+
+type Drive = Start & Move
+
+let nissanCar: Drive = {
+	engineOn: true,
+	driverName: "jason",
+	startPoint: 0,
+	endPoint: 83,
+}
+
+console.log(`${nissanCar.driverName} is driving the car.`)
+console.log(`The car is ${nissanCar.endPoint} km away from home.`)
